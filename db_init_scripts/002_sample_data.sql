a2b12a9e9d5ef7bb4922cdca62151345c01be135
cccchhhhrrrr <52762373+cccchhhhrrrr@users.noreply.github.com>
2020-12-25 23:51:51 +0100

    New sample data.

--- db_init_scripts/002_sample_data.sql
+++ db_init_scripts/002_sample_data.sql
@@ -1,22 +1,28 @@
 /* Inserts og updates til test */
-INSERT INTO Players (PlayerName, PlayerPassword, Token, IsAdmin) VALUES ('Chrelle', md5("pass"), "RANDOM_STRING", True), ('Emil', md5("pass"), "DIFFERENT_RANDOM_STRING", True);
+INSERT INTO Players (PlayerName, PlayerPassword, Token, IsAdmin) VALUES 
+    ('Chrelle', md5("pass"), "RANDOM_STRING", True)
+   ,('Emil', md5("pass"), "DIFFERENT_RANDOM_STRING", True);
 
-INSERT INTO Quizzes (StartYear, EndYear, CreatedByPlayerID) VALUES (2019, 2020, 1);
-INSERT INTO Games (GamePassword, QuizID, HostedByPlayerID) VALUES ('pass', 1, 1);
+INSERT INTO Quizzes (StartYear, EndYear, CreatedByPlayerID) VALUES 
+   (2014, 2020, 1);
+   
+INSERT INTO Games (GamePassword, QuizID, HostedByPlayerID) VALUES 
+   ('gudbevaredanmark', 1, 1);
 
-UPDATE QuizAnswers
-SET Artist = 'Eminem', Title = 'Godzilla', SongNumber = 1
-WHERE Year = 2020 AND QuizID = 1;
+CREATE TEMPORARY TABLE QuizAnswersTemp (Year INT(4), Artist VARCHAR(50), Title VARCHAR(50), SongNumber INT(2));
+INSERT INTO QuizAnswersTemp VALUES
+    (2014, 'Angel Olsen', 'White Fire', 2)
+   ,(2015, 'Joanna Newsom', 'You Will Not Take My Heart Alive', 3)
+   ,(2016, 'Solange', 'Cranes in the Sky', 1)
+   ,(2017, 'Lorde', 'Homemade Dynamite', 7)
+   ,(2018, 'Mitski', 'Two Slow Dancers', 4)
+   ,(2019, 'Lana Del Rey', 'The Greatest', 6)
+   ,(2020, 'Adrianne Lenker', 'Anything', 5);
 
-UPDATE QuizAnswers
-SET Artist = 'Weyes Blood', Title = 'Andromeda', SongNumber = 2
-WHERE Year = 2019 AND QuizID = 1;
-
-/* Uncomment to prepopulate answers
-INSERT INTO PlayerAnswers (GameID, PlayerID, SongNumber, Year) VALUES 
-   (1, 1, 1, 1998)
-  ,(1, 1, 2, 2019)
-  ,(1, 2, 1, 2018)
-  ,(1, 2, 2, 2017)
-;
-*/
+UPDATE QuizAnswers qa
+INNER JOIN QuizAnswersTemp temp
+  ON  qa.QuizID = 1 AND qa.Year = temp.Year
+SET 
+   qa.Artist = temp.Artist
+  ,qa.Title = temp.Title
+  ,qa.SongNumber = temp.SongNumber;
